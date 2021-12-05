1719e5b89b7c4e934baf035560b41f2aa8624961
Nelson Dominguez <ndominguez.riveras@gmail.com>
2021-12-05 17:54:22 -0300

    servidor heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  url: 'http://192.168.1.85:3000'
+  //url: 'http://192.168.1.85:3000'
   //url: 'http://34.176.5.244:443'
-  //url: 'http://localhost:3000'
+  url: 'http://localhost:3000'
+  //url: 'https://lit-tor-35180.herokuapp.com'
 };
 
 /*
